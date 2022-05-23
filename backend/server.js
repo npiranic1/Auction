2eab25f0c37569a9917a15478132e2226e577575
NeiraPiranic <npiranic1@etf.unsa.ba>
2022-05-23 16:38:58 +0200

    cors added, and port changed

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require('path');
+const cors = require('cors');
 
 const {db} = require('./config/db');
 // import controllers 
@@ -11,10 +12,12 @@ const app = express();
 app.use(express.static(path.join(__dirname, "public")));
 app.use(urlencodedParser);
 app.use(bodyParser.json());
+// for communication beetween servers
+app.use(cors());
 // routes
 app.use(product);
 
 db.sync(() => console.log(`Tables created!`));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3030;
 app.listen(PORT, () => console.log(`Server is running on PORT: ${PORT}`));
