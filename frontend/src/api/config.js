bae894091311e415e7f5669bfd7d5562ee75291e
NeiraPiranic <npiranic1@etf.unsa.ba>
2022-06-19 18:03:21 +0200

    base url changed

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,6 +1,5 @@
 import { getToken } from "utility/storageService";
-//export const BASE_URL = process.env.REACT_APP_URL
-export const BASE_URL = "http://localhost:3030"
+export const BASE_URL = process.env.REACT_APP_URL
 
 export function getHeaders(){
     const token = getToken();
